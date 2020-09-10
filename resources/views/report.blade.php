891b67086d23db082b87124422300650f2be0569
Khalid Abdurrahman <omandotkom@gmail.com>
2020-09-10 23:00:29 +0700

    resolved #4

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -8,7 +8,7 @@
 
 @section('content')
 <div class="card w-75 mx-auto">
-    <h5 class="card-header">Daftar Data Kunjungan</h5>
+    <h5 class="card-header">Daftar Data Lapoan</h5>
     <div class="card-body">
         <table class="table table-responsive-md">
             <thead>
@@ -27,7 +27,7 @@
                     <td>{{$visit->namakegiatan}}</td>
                     <td>{{$visit->tanggal}}</td>
                     <td>{{$visit->updated_at}}</td>
-                    <td><a href="{{route('edit-laporan',['id'=>$visit->id])}}" class="badge badge-success"><i class="fas fa-plus-circle"></i></a></td>
+                    <td><a href="{{route('edit-laporan',['id'=>$visit->id])}}" class="badge badge-success"><i class="fas fa-plus-circle"></i></a><a href="{{route('printreport',['id'=>$visit->id])}}" class="badge badge-success m-1"><i class="fas fa-print"></i></a></td>
                 </tr>
                 @endforeach
             </tbody>
